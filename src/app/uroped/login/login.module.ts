aa75fa91c3761c6a726d6658c65c42c386134124
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2022-10-23 11:41:40 -0300

    #4 adding cpf mask

--- src/app/uroped/login/login.module.ts
+++ src/app/uroped/login/login.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { MaterialModule } from './../../material.module';
 import { FormLogonComponent } from './components/form-logon/form-logon.component';
@@ -10,12 +11,18 @@ import { LogonComponent } from './pages/logon/logon.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-    declarations: [LogonComponent, FormLogonComponent, FormRegisterComponent, RegisterComponent],
+    declarations: [
+        LogonComponent,
+        FormLogonComponent,
+        FormRegisterComponent,
+        RegisterComponent,
+    ],
     imports: [
         CommonModule,
         MaterialModule,
         LoginRoutingModule,
         ReactiveFormsModule,
+        NgxMaskModule.forRoot(),
     ],
 })
 export class LoginModule {}
