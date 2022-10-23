14435dbb9e6c247f2c7309999c8c25bdf48c86fd
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2022-10-23 00:47:20 -0300

    #3 adding extras

--- src/app/shared/services/navigation.service.ts
+++ src/app/shared/services/navigation.service.ts
@@ -7,15 +7,32 @@ import { Router } from '@angular/router';
 export class NavigationService {
     constructor(private router: Router) {}
 
-    navigate(path: string[]) {
-        this.router.navigate(path).then(
-            (nav) => {
-                console.log(':: Navigate :: Navigating to route:', path);
-            },
-            (err) => {
-                console.log(err);
-                this.router.navigate(['error', '404']);
-            }
-        );
+    navigate(path: string[], extras?: any) {
+        if (extras) {
+            this.router.navigate(path, extras).then(
+                (nav) => {
+                    console.log(
+                        ':: Navigate :: Navigating to route:',
+                        path,
+                        'with extras:',
+                        extras
+                    );
+                },
+                (err) => {
+                    console.log(err);
+                    this.router.navigate(['error', '404']);
+                }
+            );
+        } else {
+            this.router.navigate(path).then(
+                (nav) => {
+                    console.log(':: Navigate :: Navigating to route:', path);
+                },
+                (err) => {
+                    console.log(err);
+                    this.router.navigate(['error', '404']);
+                }
+            );
+        }
     }
 }
