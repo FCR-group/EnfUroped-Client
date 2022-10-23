5f70066711c0d3f35208387183ddbbf1c118c3b1
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2022-10-23 00:46:25 -0300

    #7 Criar formulário de login

--- src/app/uroped/login/components/form-logon/form-logon.component.ts
+++ src/app/uroped/login/components/form-logon/form-logon.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'form-logon',
@@ -6,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./form-logon.component.scss'],
 })
 export class FormLogonComponent implements OnInit {
-    constructor() {}
+    form!: FormGroup;
+    msgCpf: string = '';
 
-    ngOnInit(): void {}
+    @Output() onSubmit: EventEmitter<any> = new EventEmitter();
+    @Output() onSignIn: EventEmitter<any> = new EventEmitter();
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.createForm();
+    }
+
+    createForm() {
+        this.form = this.formBuilder.group({
+            id: new FormControl(null, [Validators.required]),
+            password: new FormControl(null, [Validators.required]),
+        });
+    }
+
+    register() {
+        this.onSignIn.emit(true);
+    }
+
+    send() {
+        if (this.form.valid) {
+            this.onSubmit.emit(this.form.value);
+        }
+    }
 }
